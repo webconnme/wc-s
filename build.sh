9f433cf4dc563ae581e93076716830e177c5dd80
Minsoo Kim <leios@falinux.com>
2015-10-05 11:25:47 +0900

    docker image에 필요한 패키지들 기본 설치

--- build.sh
+++ build.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
-go get github.com/go-martini/martini
-go get github.com/martini-contrib/binding
-go get github.com/martini-contrib/render
-go get github.com/martini-contrib/cors
-
-go get --tags zmq_4_x github.com/alecthomas/gozmq
-go get github.com/mikepb/go-serial
-
 cd src
 for MODULE in wc-s/app/*
 do
+	echo build $MODULE
 	go install $MODULE
 done
 
 for MODULE in wc-s/env/*
 do
+	echo build $MODULE
 	go install $MODULE
 done
+cd ..
+
+cd native
+make -C led
+cd ..
